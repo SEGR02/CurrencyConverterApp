467164ea02b41fafcadd329f49ba98d860a77f85
SEGR02 <sebastian.gomz02@gmail.com>
2023-07-13 01:06:17 -0400

    test with navigation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import "./assets/styles/normalize.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, VueRouter } from "vue-router";
 import App from "./App.vue";
 import Login from "./components/Login.vue";
 import Home from "./components/Home.vue";
@@ -12,10 +12,10 @@ const routes = [
   { path: "/login", component: Login },
 ];
 
-const router = new VueRouter({
-  mode: "",
-  base: "https://main--ornate-faloodeh-e23110.netlify.app",
-  routes,
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
 });
 
 const app = createApp(App);
