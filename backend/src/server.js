f69af2a04d2e1fcd2f8458f0f6e361a319c1d87e
SEGR02 <sebastian.gomz02@gmail.com>
2023-07-12 22:50:43 -0400

    ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
-app.listen(8000, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor o servidora corriendo en el puerto ${PORT}`);
 });
