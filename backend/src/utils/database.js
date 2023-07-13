cdbfc9736dc273e4a0bd0ced90247869f25a415b
SEGR02 <sebastian.gomz02@gmail.com>
2023-07-12 22:42:29 -0400

    feat: bug fixes for deployment

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -5,7 +5,7 @@ const db = new Sequelize({
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   host: process.env.DB_HOST,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   dialect: "postgres",
   logging: false,
